8ddec5432b16111a7e771052c4125ccd10fbd680
Felipe Cruz Bonilla <felipecruzbonilla@gmail.com>
2022-07-04 16:43:57 -0400

    test2

--- mediaqueries2/assets/js/main.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-    var x = document.getElementById("menu-burger");
-    if (x.style.display === "block") {
-        x.style.display = "none";
-    } else {
-        x.style.display = "block";
-    }
-}
